650b7c40d6774b55d7fa2d75e1fa1e442a8a8130
sincaster <sindavmi@gmail.com>
2020-05-12 20:31:22 +0300

    cock

---
+++ suckless_dmenu/config.h
@@ -0,0 +1,25 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+	   centered = 0;
+	   min_width = 350; 
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"Cozette Vector:size=14"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#6C71C4", "#002B36" },
+	[SchemeSel] = { "#6C71C4",  "#FDF6E3" },
+	[SchemeOut] = { "#6C71C4", "#00ffff" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 15;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = "⚝";
